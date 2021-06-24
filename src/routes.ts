48d0cb72a557333c7b7acd765878eb17f3673e94
Marçal Marcos <42890965+MarcosMarcal@users.noreply.github.com>
2021-06-24 06:56:34 -0300

    aplicando middleware

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express"
 import { UserController } from "./controller/UserController"
 import { TagController } from "./controller/TagController"
+import { ensureAdmin } from "./middleware/ensureAdmin"
 
 const router = Router()
 
@@ -8,7 +9,7 @@ const userController = new UserController()
 const tagController = new TagController()
 
 router.post("/createUser", userController.createUser)
-router.post("/createTag", tagController.createTag)
+router.post("/createTag", ensureAdmin, tagController.createTag)
 
 router.get("/healthCheck", (req, res) => {
     console.log("...healthCheck... running...")
