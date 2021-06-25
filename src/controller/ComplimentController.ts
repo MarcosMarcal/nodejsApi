d4affa85919315c7b61a52ed29d9f8e1e926b722
Marçal Marcos <42890965+MarcosMarcal@users.noreply.github.com>
2021-06-24 23:17:11 -0300

    middleware de autenticacao

--- src/controller/ComplimentController.ts
+++ src/controller/ComplimentController.ts
@@ -4,12 +4,13 @@ import { ComplimentService } from "../service/ComplimentService";
 class ComplimentController {
 
     async createCompliment(req: Request, res: Response) {
-        const { tag_id, user_sender, user_receiver, message } = req.body
+        const { tag_id, user_receiver, message } = req.body
+        const { user_id } = req
 
         const complimentService = new ComplimentService()
 
         const compliment =  await complimentService.createCompliment({
-            tag_id, user_sender, user_receiver, message
+            tag_id, user_sender: user_id, user_receiver, message
         })
 
         return res.json(compliment)
--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { TagController } from "./controller/TagController"
 import { ensureAdmin } from "./middleware/ensureAdmin"
 import { AuthenticateController } from "./controller/AuthenticateController"
 import { ComplimentController } from "./controller/ComplimentController"
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated"
 
 const router = Router()
 
@@ -14,8 +15,8 @@ const complimentController = new ComplimentController()
 
 router.post("/authUser", authenticateController.authenticate)
 router.post("/createUser", userController.createUser)
-router.post("/createTag", ensureAdmin, tagController.createTag)
-router.post("/createCompliment", complimentController.createCompliment)
+router.post("/createTag", ensureAuthenticated, ensureAdmin, tagController.createTag)
+router.post("/createCompliment", ensureAuthenticated, complimentController.createCompliment)
 
 router.get("/healthCheck", (req, res) => {
     console.log("...healthCheck... running...")
